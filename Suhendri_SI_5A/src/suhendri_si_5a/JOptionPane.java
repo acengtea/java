a8627fd5ee332e54150f67b34632c969ed821ea5
acengtea <47108998+acengtea@users.noreply.github.com>
2019-01-28 19:52:15 +0700

    Add files via upload

---
+++ Suhendri_SI_5A/src/suhendri_si_5a/JOptionPane.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package suhendri_si_5a;
+
+/**
+ *
+ * @author G400-1215
+ */
+class JOptionPane {
+    
+}
